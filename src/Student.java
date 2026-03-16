6a561464b836ffbf419bcf247f39605fd8cd64db
AlexM-058 <alexandrumateitarita@gmail.com>
2026-03-17 01:38:17 +0200

    Tema casa laborator 2

--- src/Student.java
+++ src/Student.java
@@ -6,18 +6,15 @@ public class Student {
     protected  String prenume;
     protected  String nume;
     protected   String formatieDeStudiu;
-    protected static List<Student> Catalog = new ArrayList<>();
+    protected static Set<Student> Catalog = new HashSet<>();
 
-    public Student(int numarMatricol, String prenume, String nume, String formatieDeStudiu, int add) {
+    public Student(int numarMatricol, String prenume, String nume, String formatieDeStudiu) {
         this.numarMatricol = numarMatricol;
         this.prenume = prenume;
         this.nume = nume;
         this.formatieDeStudiu = formatieDeStudiu;
-        if (add == 1) {
-            Catalog.add(this);
-        } 
-        
     }
+
     public static void afisareCatalog() {
         for (Student s : Catalog) {
             System.out.println(s);
@@ -25,14 +22,7 @@ public class Student {
 
     }
     public static boolean Check_student(Student student) {
-        for (Student s : Catalog) {
-            if (s.numarMatricol == student.numarMatricol
-                    && s.formatieDeStudiu == student.formatieDeStudiu
-            && s.prenume == student.prenume && s.nume == student.nume) {
-                return true;
-            }
-        }
-        return false;
+        return Catalog.contains(student);
     }
 
     @Override
@@ -44,23 +34,37 @@ public class Student {
                 " formatieDeStudiu='" + formatieDeStudiu + '\''
                 ;
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return (numarMatricol == student.numarMatricol && prenume.equals(student.prenume)
+                && nume.equals(student.nume) && formatieDeStudiu.equals(student.formatieDeStudiu));
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(numarMatricol, prenume, nume, formatieDeStudiu);
+    }
+
+
     public static void main(String[] args) {
 //        Student student =new Student(120, "Alis", "Popa", "TI21/2");
-        Student student0 = new Student(112, "Maria", "Popa", "TI21/1",1);
-        Student student1 = new Student(112, "Maria", "Popa", "TI21/1",1);
-        Student student2 = new Student(113, "Andrei", "Ionescu", "TI21/1",1);
-        Student student3 = new Student(114, "Elena", "Georgescu", "TI21/1",1);
-        Student student4 = new Student(115, "Vlad", "Dumitrescu", "TI21/2",1);
-        Student student5 = new Student(116, "Ioana", "Stan", "TI21/2",1);
-        Student student6 = new Student(117, "Radu", "Marin", "TI21/2",1);
-        Student student7 = new Student(118, "Ana", "Petrescu", "TI21/3",1);
-        Student student8 = new Student(119, "Mihai", "Enache", "TI21/3",1);
-        Student student9 = new Student(120, "Bianca", "Ilie", "TI21/3",1);
-        Student student10 = new Student(121, "Stefan", "Matei", "TI21/1",1);
+//        Student student0 = new Student(112, "Maria", "Popa", "TI21/1",1);
+        Catalog.add(new Student(112, "Maria", "Popa", "TI21/1"));
+        Catalog.add(new Student(113, "Andrei", "Ionescu", "TI21/1"));
+        Catalog.add(new Student(114, "Elena", "Georgescu", "TI21/1"));
+        Catalog.add(new Student(115, "Vlad", "Dumitrescu", "TI21/2"));
+        Catalog.add(new Student(116, "Ioana", "Stan", "TI21/2"));
+        Catalog.add(new Student(117, "Radu", "Marin", "TI21/2"));
+        Catalog.add(new Student(118, "Ana", "Petrescu", "TI21/3"));
+        Catalog.add(new Student(119, "Mihai", "Enache", "TI21/3"));
+        Catalog.add(new Student(120, "Bianca", "Ilie", "TI21/3"));
+        Catalog.add( new Student(121, "Stefan", "Matei", "TI21/1"));
 
 
-        Student cautat = new Student(112, "Maria", "Popa", "TI21/1",0);
-        Student cautat2 = new Student(120, "Alis", "Popa", "TI21/2",0);
+        Student cautat = new Student(112, "Maria", "Popa", "TI21/1");
+        Student cautat2 = new Student(120, "Alis", "Popa", "TI21/2");
 
         System.out.println("\nCatalogul:");
         afisareCatalog();
