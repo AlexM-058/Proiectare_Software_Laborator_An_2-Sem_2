3e7d6e31f342e97c464489c1c880e8455265ecca
AlexM-058 <alexandrumateitarita@gmail.com>
2026-03-25 09:16:48 +0200

    feat: add initial lab 4 package + tanar class created

--- src/lab_4/AppLab4.java
+++ src/lab_4/AppLab4.java
@@ -1,11 +1,29 @@
 package lab_4;
 
 import java.io.IOException;
+import java.util.*;
 
 public class AppLab4 {
 
     static void main(String args[]) throws IOException {
 
+        HashMap<String, Integer> varste = new HashMap<>();
+        varste.put("Ioan", 21);
+        varste.put("Maria", 22);
+        varste.put("Victor", 20);
+        varste.put("Simina", 20);
+        varste.put("Marius", 21);
+        varste.put("Mihai", 21);
+        varste.put("Daniela", 23);
+        Map<String, String> adrese = Map.of("Ioan", "Sibiu", "Maria", "Bucuresti", "Victor",
+                "Cluj","Simina", "Alba-Iulia","Marius", "Medias", "Mihai", "Cisnadie","Daniela", "Sibiu");
+        varste.put("Vlad", 19);
+        varste.put("Iulia", 19);
+        for(String key : varste.keySet()){
+            System.out.println(key + " " + varste.get(key));
+        }
+        HashMap<String,Tanar> tineri = new HashMap<>();
+//        tineri.
 
     }
 }
