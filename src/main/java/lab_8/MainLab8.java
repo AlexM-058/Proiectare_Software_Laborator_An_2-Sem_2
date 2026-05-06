28ae84c924de9e98f1d3ed14309c8efdb163d555
AlexM-058 <alexandrumateitarita@gmail.com>
2026-05-06 07:10:44 +0300

    Add lab 8 Apache POI solutions

---
+++ src/main/java/lab_8/MainLab8.java
@@ -0,0 +1,22 @@
+package lab_8;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MainLab8 {
+    private static final Path INPUT_PATH = Paths.get("Inputs", "laborator8_input.xlsx");
+    private static final Path OUTPUT2_PATH = Paths.get("Outputs", "laborator8_output2.xlsx");
+    private static final Path OUTPUT3_PATH = Paths.get("Outputs", "laborator8_output3.xlsx");
+
+    public static void main(String[] args) {
+        Lab8ExcelProcessor processor = new Lab8ExcelProcessor();
+        try {
+            processor.citesteSiAfiseazaExcel(INPUT_PATH);
+            processor.copiazaCuMedieCalculata(INPUT_PATH, OUTPUT2_PATH);
+            processor.copiazaCuFormulaMedie(INPUT_PATH, OUTPUT3_PATH);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/student/MainStudent.java
+++ src/main/java/student/MainStudent.java
@@ -2,8 +2,29 @@ package student;
 
 import student.examples.MainStudentLab5;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class MainStudent {
+    private static final Path STUDENTS_XLS_PATH = Paths.get("Outputs", "laborator8_students.xls");
+
     public static void main(String[] args) {
         MainStudentLab5.main(args);
+
+        AplicatieCuBursa aplicatie = new AplicatieCuBursa();
+        List<StudentBursier> studenti = aplicatie.genereaza();
+        StudentExcelProcessor processor = new StudentExcelProcessor();
+
+        try {
+            processor.exportaStudenti(STUDENTS_XLS_PATH, studenti);
+            List<StudentBursier> studentiCititi = processor.citesteStudenti(STUDENTS_XLS_PATH);
+            for (StudentBursier student : studentiCititi) {
+                System.out.println(student);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
